2411e056e543f5f7c6d581a3db39ea60ee5f2c49
Buggyes <matheus@chatisthisreal>
2025-06-10 22:02:46 -0300

    started making API calls for area and started developing the mainpage

--- course-management-website-source/src/components/API/AreaAPI.js
+++ course-management-website-source/src/components/API/AreaAPI.js
@@ -1,1 +1,17 @@
 import { baseURL } from "./BaseAPI";
+
+export default async function get_areas() {
+    let url = baseURL + 'areas/'
+    try {
+        const response = await fetch(url,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            })
+        return response.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
--- course-management-website-source/src/components/MainPage/MainPage.vue
+++ course-management-website-source/src/components/MainPage/MainPage.vue
@@ -1,13 +1,21 @@
 <script setup>
-    
+    import { ref } from "vue";
+    import get_areas from "../API/AreaAPI"
+
+    const areas = ref(await get_areas())
+    console.log(areas)
 </script>
 
 <template>
-<header>oi</header>
+<header><span>oi</span></header>
+
 </template>
 
 <style lang="css" scoped>
 header {
     background-color: black;
+}
+header span {
+    color: white;
 }
 </style>
